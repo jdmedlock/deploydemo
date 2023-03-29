92a0f7994fc94acb3e998a8994fa2f8e28bb4ad2
jdmedlock <jdmedlock@gmail.com>
2023-03-29 16:40:06 -0500

    Feature: Add landing page subtitle

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello Chingu World!</h2>
+        <h2>Hello Chingu World</h2>
+        <h3>Have fun deploying me!</h3>
       </header>
     </div>
   );
