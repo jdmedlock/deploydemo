259ec852f4c0cb143ce1cee4fda419693c27adcf
jdmedlock <jdmedlock@gmail.com>
2023-03-29 17:35:10 -0500

    Feature: Add landing page text

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello Chingu World</h2>
         <h3>Have fun deploying me!</h3>
+        <p>This app will be used in the Chingu Roundtable to demonstrate app deployments.</p>
       </header>
     </div>
   );
